dc7510c29fa226953fa239040b2b4489572b9dd3
micah riley <micahriley88@gmail.com>
2019-09-23 18:04:59 -0400

    Added remaining basic input fields for CreateEvent component.

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -16,6 +16,20 @@ function CreateEvent () {
     return (
         <form>
             <input type="text" name="eventName" onChange={handleChanges} placeholder="Enter Event Name" value={input.eventName} />
+            <input type="text" name="image" onChange={handleChanges} placeholder="Enter Photo URL" value={input.eventName} />
+            <div className="location">
+                <input type="text" name="address" onChange={handleChanges} placeholder="Address" value={input.address}/>  
+                <input type="text" name="city" onChange={handleChanges} placeholder="City" value={input.city}/>  
+                <input type="text" name="state" onChange={handleChanges} placeholder="State" value={input.state}/>
+                <input type="number" name="postal" onChange={handleChanges} placeholder="Postal Code" value={input.postal}/>
+            </div>
+            <div className="description">
+                <textarea cols="100" name="eventDescription" onChange={handleChanges} placeholder="Enter Event Description" value={input.eventName} />
+            </div>
+            <div className="vendor">
+                <input type="text" name="vendor" onChange={handleChanges} placeholder="Enter Vendor" value={input.vendor} />
+            </div>
+            <button>Submit</button>
         </form>
     )
 }
