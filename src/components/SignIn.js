eb6cb2a481dddef1457f2762ea000a5ebb989dde
chudnofskyATD <mpereannor@gmail.com>
2019-09-23 22:30:42 +0000

    Sign In Component v2

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,13 +1,27 @@
 import React, {useState} from 'react';
 
+const infoSignIn = {
+    username: "",
+    password: ""
+}
+
+
 
 export default function SignIn () {
-    const[userInfo, setUserInfo] = useState({ username: "" , password: ""});
+    const[userInfo, setUserInfo] = useState({infoSignIn});
+
+    const handleChange = e => {
+        setUserInfo({...userInfo, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        setUserInfo({infoSignIn}) 
+    }
 
     return (
-    <form >
-        <input type='text' name="username" placeholder="username" />
-        <input type='password' name="password" placeholder="password" />
+    <form  onSubmit ={handleSubmit}>
+        <input type='text' name="username" placeholder="Enter username"  onChange ={handleChange} value={userInfo.username}/>
+        <input type='password' name="password" placeholder="password" onChange ={handleChange} value={userInfo.password} />
         <button type='submit'>Sign in</button>
     </form>
     )
