497b9051569ee8afc4a5cdbe1c0c4200b750202f
micah riley <micahriley88@gmail.com>
2019-09-23 17:48:32 -0400

    Added handleChanges and submitForm functions for CreateEvent component. Added first input field to form.

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 
 function CreateEvent () {
-    const [input, setInput] = useState();
+    const [input, setInput] = useState("");
     console.log({input}, "input results in a cleared state")
+    const handleChanges = e => {
+        setInput({...input, [e.target.name]: e.target.value});
+        console.log({input}, "This is the results of notes after handleChanges setInput")
+    }
+    const submitForm = e => {
+        e.preventDefault();
+        // Put a props reference here to set useState to input for parent component
+        console.log({input});
+        setInput("")
+    }
     return (
         <form>
-            
+            <input type="text" name="eventName" onChange={handleChanges} placeholder="Enter Event Name" value={input.eventName} />
         </form>
     )
 }
