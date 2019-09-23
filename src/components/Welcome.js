c4f2ab7b5c4d9f035a52483b3fec2568700e8e9f
chudnofskyATD <mpereannor@gmail.com>
2019-09-23 22:44:18 +0000

    Welcome Component v1

---
+++ src/components/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Welcome () {
+
+
+    return (
+        <form>
+          <button>Sign In</button>
+          <button>Guest</button>
+          <button>Sign Up</button>
+        </form>
+
+    )
+}
