639289b505da6047a74588438eb4711848863045
Devin Dias <devindias.tech@gmail.com>
2019-09-24 11:45:38 -0500

    log in sign up

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,9 +9,9 @@ export const LOG_USER_START = "LOG_USER_START"
 export const LOG_USER_SUCCESS = "LOG_USER_SUCCESS"
 export const LOG_USER_FAILURE = "LOG_USER_FAILURE"
 
-export const EDIT_USER_START = "EDIT_USER_START"
-export const EDIT_USER_SUCCESS = "EDIT_USER_SUCCESS"
-export const EDIT_USER_FAILURE = "EDIT_USER_FAILURE"
+// export const EDIT_USER_START = "EDIT_USER_START"
+// export const EDIT_USER_SUCCESS = "EDIT_USER_SUCCESS"
+// export const EDIT_USER_FAILURE = "EDIT_USER_FAILURE"
 
 export const registerUser = (user,history) => {
       return dispatch => {
@@ -30,7 +30,7 @@ export const logIn = (user,history) => {
       return dispatch => {
             dispatch({type: LOG_USER_START});
                   axios.post(
-                        "*http://linkhere*",user
+                        "http://linkhere",user
                   ).then(res => {
                         dispatch({type: LOG_USER_SUCCESS, payload: res.data})
                         history.push("/portfolio:id")
@@ -39,29 +39,29 @@ export const logIn = (user,history) => {
                   })
       }
 }
-export const editCard = (card,history) => {
-      return dispatch => {
-            dispatch({type: Edit_USER_START});
-                  axios.post(
-                        "*http://linkhere*",card
-                  ).then(res => {
-                        dispatch({type: Edit_USER_SUCCESS, payload: res.data})
-                        history.push("/portfolio:id")
-                  }).catch(error => {
-                        dispatch({type: Edit_USER_FAILURE, payload: error.data})
-                  })
-      }
-}
-export const deleteCard = (card,history) => {
-      return dispatch => {
-            dispatch({type: LOG_USER_START});
-                  axios.post(
-                        "*http://linkhere*",card
-                  ).then(res => {
-                        dispatch({type: LOG_USER_SUCCESS, payload: res.data})
-                        history.push("/portfolio:id")
-                  }).catch(error => {
-                        dispatch({type: LOG_USER_FAILURE, payload: error.data})
-                  })
-      }
-}
+// export const editCard = (card,history) => {
+//       return dispatch => {
+//             dispatch({type: Edit_USER_START});
+//                   axios.post(
+//                         "*http://linkhere*",card
+//                   ).then(res => {
+//                         dispatch({type: Edit_USER_SUCCESS, payload: res.data})
+//                         history.push("/portfolio:id")
+//                   }).catch(error => {
+//                         dispatch({type: Edit_USER_FAILURE, payload: error.data})
+//                   })
+//       }
+// }
+// export const deleteCard = (card,history) => {
+//       return dispatch => {
+//             dispatch({type: LOG_USER_START});
+//                   axios.post(
+//                         "*http://linkhere*",card
+//                   ).then(res => {
+//                         dispatch({type: LOG_USER_SUCCESS, payload: res.data})
+//                         history.push("/portfolio:id")
+//                   }).catch(error => {
+//                         dispatch({type: LOG_USER_FAILURE, payload: error.data})
+//                   })
+//       }
+// }
