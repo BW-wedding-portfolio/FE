8a8a2ab0ba18a5f182647c3440d4a6770493c826
micah riley <micahriley88@gmail.com>
2019-09-24 20:18:05 -0400

    Remerging

--- src/App.js
+++ src/App.js
@@ -12,23 +12,15 @@ function App() {
   return (
     <div className="App">
       
-      <Route exact path="/" component={Welcome} />
+      {/* <Route exact path="/" component={Welcome} />
       <Route path="/signin" component={SignIn} />
-      <Route path="/signup" component={SignUp} />
+      <Route path="/signup" component={SignUp} /> */}
       {/* <SignIn/> */}
       {/* <Welcome/> */}
       {/* <SignUp /> */}
-<<<<<<< HEAD
-<<<<<<< HEAD
-      {/* <CreateEvent /> */}
-      {/* <GuestPortfolio /> */}
-=======
->>>>>>> 6b307f9922ab35744dcf43d77d334db325a5960b
-=======
 
       {/* <CreateEvent /> */}
-      {/* <GuestPortfolio /> */}
->>>>>>> 306b284798ce4855cbc449f1bc8c6f74de74aeb7
+      <GuestPortfolio />
     </div>
   );
 }
