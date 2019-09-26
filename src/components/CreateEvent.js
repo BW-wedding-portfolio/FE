2a843f8cbf1e22cac755e5f7cf7463fad7dcdc75
Devin Dias <devindias.tech@gmail.com>
2019-09-26 13:05:11 -0500

    completed delete

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,70 +1,118 @@
 import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-function CreateEvent () {
-    const [input, setInput] = useState("");
-    console.log({input}, "input results in a cleared state")
-    const [vendor, setVendor] = useState([])
-    const handleChanges = e => {
-        setInput({...input, [e.target.name]: e.target.value});
-        console.log({input}, "This is the results of notes after handleChanges setInput")
-    }
-    const handleChangesVendor = e => {
-        setInput({...vendor, [e.target.name]: e.target.value});
-        console.log({vendor}, "This is the results of notes after handleChanges setInput")
-    }
-    const submitForm = e => {
-        e.preventDefault();
-        setVendor([]);
-        // Put a props reference here to set useState to input for parent component
-        // console.log({input});
-        setInput(
-            {
-                eventName: "",
-                image: "",
-                address: "",
-                city: "",
-                state: "",
-                postal: "",
-                eventDescription: "",
-                vendor: ""
-            }
-        )
-    }
-    const addVendor = e => {
-        e.preventDefault();
-        // Put a props reference here to set useState to input for parent component
-        console.log(e);
-        setVendor([...vendor, input.vendor])
-        setInput({vendor:""})
-    }
-    const deleteVendor = e => {
-        e.preventDefault();
-        setVendor(vendor.slice(0, vendor.length-1))
-    }
+function CreateEvent(props) {
+  const [input, setInput] = useState({
+    event_description: "",
+    event_location: "",
+    event_name: "",
+    img_url: "",
+    theme: "",
+    vendors: ""
+  });
+  // console.log({input}, "input results in a cleared state")
 
-    return (
-        <>
-            <form onSubmit={submitForm}>
-                <input type="text" name="eventName" onChange={handleChanges} placeholder="Enter Event Name" value={input.eventName} />
-                <input type="text" name="image" onChange={handleChanges} placeholder="Enter Photo URL" value={input.image} />
-                <div className="location">
-                    <input type="text" name="address" onChange={handleChanges} placeholder="Address" value={input.address}/>  
-                    <input type="text" name="city" onChange={handleChanges} placeholder="City" value={input.city}/>  
-                    <input type="text" name="state" onChange={handleChanges} placeholder="State" value={input.state}/>
-                    <input type="number" name="postal" onChange={handleChanges} placeholder="Postal Code" value={input.postal}/>
-                </div>
-                <div className="description">
-                    <textarea cols="100" name="eventDescription" onChange={handleChanges} placeholder="Enter Event Description" value={input.eventDescription} />
-                </div>
-                <form>
-                    <input type="text" name="vendor" onChange={handleChangesVendor} placeholder="Enter Vendor" value={input.vendor} />
-                    <button onClick={addVendor}>Add Vendor</button><button onClick={deleteVendor}>Delete</button>
-                    {vendor.map(e => (<p>{e}</p>))}
-                </form>
-                <button>Submit</button>
-            </form>
-        </>
-    )
+  const handleChanges2 = e => {
+    console.log("I am in handleChanges2).");
+    setInput({ ...input, [e.target.name]: e.target.value });
+    // console.log({input}, "This is the results of notes after handleChanges setInput")
+    console.log("handleChanges2 Input: ", input);
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .get(`https://wedding-portfolio-bw.herokuapp.com/planners/`)
+      .then(res => {
+        console.log(res);
+        const id = res.data.id;
+        axiosWithAuth()
+          .post(
+            `https://wedding-portfolio-bw.herokuapp.com/events/${id}/events`,
+            input
+          )
+          .then(res => {
+            console.log("Post Event: ", res);
+            props.history.push("/userportfolio");
+          })
+          .catch(err => console.log("Post Event Error: ", err.respone));
+      });
+
+    setInput({
+      event_description: "",
+      event_location: "",
+      event_name: "",
+      img_url: "",
+      theme: "",
+      vendors: ""
+    });
+  };
+  // const addVendor = e => {
+  //     e.preventDefault();
+  //     // Put a props reference here to set useState to input for parent component
+  //     console.log(e);
+  //     setVendor([...vendor, input.vendor])
+  //     setInput({vendor:""})
+  // }
+  // const deleteVendor = e => {
+  //     e.preventDefault();
+  //     setVendor(vendor.slice(0, vendor.length-1))
+  // }
+
+  return (
+    <>
+      <form onSubmit={submitForm}>
+        {console.log("Test: ", input.event_name)}
+        <input
+          type="text"
+          name="event_name"
+          onChange={handleChanges2}
+          placeholder="Enter Event Name"
+          value={input.event_name}
+        />
+        <input
+          type="text"
+          name="img_url"
+          onChange={handleChanges2}
+          placeholder="Enter Photo URL"
+          value={input.img_url}
+        />
+
+        <input
+          type="text"
+          name="event_location"
+          onChange={handleChanges2}
+          placeholder="City"
+          value={input.event_location}
+        />
+        <input
+          type="text"
+          name="theme"
+          onChange={handleChanges2}
+          placeholder="City"
+          value={input.theme}
+        />
+        <textarea
+          cols="100"
+          name="event_description"
+          onChange={handleChanges2}
+          placeholder="Enter Event Description"
+          value={input.event_description}
+        />
+
+        <input
+          type="text"
+          name="vendors"
+          onChange={handleChanges2}
+          placeholder="Enter Vendors"
+          value={input.vendors}
+        />
+
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default CreateEvent;
